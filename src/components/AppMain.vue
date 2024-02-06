65ea76cd4ef23a433af7174b498541330958be1d
dieghino26 <brotzadiego@gmail.com>
2024-02-07 00:30:58 +0100

    created initial blocks

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import PokemonList from './PokemonList.vue';
+export default {
+    name: "AppMain",
+    components: { PokemonList }
+}
+
+</script>
+
+<template>
+    <main class="container">
+        <PokemonList />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    background-color: grey;
+
+}
+</style>
+
