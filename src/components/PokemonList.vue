45200c09442ca80d431f775e29ee4b59d4f6edea
dieghino26 <brotzadiego@gmail.com>
2024-02-07 00:59:50 +0100

    added pokemon call

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,10 +1,21 @@
 <script>
 import axios from "axios";
+const endpoint = "https://pokeapi.co/api/v2/pokemon"
 export default {
-    name: "PokemonList"
+    name: "PokemonList",
+    data: () => ({
+        pokemon: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.pokemon = res.data.results;
+        })
+    }
 };
 </script>
 
 <template>
-    <section id="pokemon"></section>
+    <section id="pokemon">
+
+    </section>
 </template>
